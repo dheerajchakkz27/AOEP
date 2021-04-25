378de382bfa8ad9c11d8bde0114c98a236fb2627
adarsh-av13 <dra4474@gmail.com>
2021-04-25 14:10:57 +0530

    client login register components added

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  {
+    path: "", component: AppComponent, children: [
+      { path: "", component: RegisterComponent },
+      { path: "login", component: LoginComponent }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
